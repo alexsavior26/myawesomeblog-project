64a0d421d672568ccfb82ba8e8278dfb88dadfbe
Savior <alex94sav@gmail.com>
2020-11-29 13:10:28 +0900

    Project done

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Post(models.Model):
     post_date = models.DateTimeField()
     post_text = models.CharField(max_length=300)
     post_image = models.ImageField(upload_to='post_images/')
+
+    def get_summary(self):
+        return self.post_text[:50]
+
+    def __str__(self):
+        return self.post_title
