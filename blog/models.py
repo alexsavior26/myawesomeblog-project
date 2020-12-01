3f4783ef48640333d510b87215e2d344f24007a6
Savior <alex94sav@gmail.com>
2020-12-01 10:38:49 +0900

    finish ver

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,11 @@ class Post(models.Model):
     post_title = models.CharField(max_length=50)
     post_date = models.DateTimeField()
     post_text = models.CharField(max_length=300)
+    post_fulltext = models.CharField(max_length=1500, null=True)
     post_image = models.ImageField(upload_to='post_images/')
 
     def get_summary(self):
-        return self.post_text[:50]
+        return self.post_text[:65]
 
     def __str__(self):
         return self.post_title
